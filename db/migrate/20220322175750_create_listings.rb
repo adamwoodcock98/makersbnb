c6edac1e6db1439b946549a15a77f26acfee4e52
Paul <pkilgarriff@gmail.com>
2022-03-23 13:48:59 +0000

    Update names of listing columns

--- db/migrate/20220322175750_create_listings.rb
+++ db/migrate/20220322175750_create_listings.rb
@@ -3,8 +3,8 @@ class CreateListings < ActiveRecord::Migration[7.0]
     create_table :listings do |t|
       t.string :name
       t.string :description
-      t.integer :price
-      t.integer :host_user_id
+      t.integer :pence_price
+      t.integer :host_id
     end
   end
 end
